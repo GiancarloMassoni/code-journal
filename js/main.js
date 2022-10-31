89e6d8bd72803efcd6f88f5afd0f2091544e9419
Giancarlo Massoni <massonicarlo55@gmail.com>
2022-10-31 21:40:59 +0000

    added submit events and object with input values

--- js/main.js
+++ js/main.js
@@ -1,10 +1,24 @@
-var $photoUrl = document.querySelector('#photo-url');
+var $photoUrl = document.querySelector('#photourl');
 var $photoOutput = document.querySelector('img');
+var $form = document.querySelector('.entry-form');
+
 $photoUrl.addEventListener('input', updatePhoto);
+$form.addEventListener('submit', updateForm);
 
 function updatePhoto(event) {
-
   if ($photoUrl !== '') {
     $photoOutput.setAttribute('src', event.target.value);
   }
+}
+
+function updateForm(event) {
+  event.preventDefault();
+  var formInputs = {};
+  var title = document.getElementById('title');
+  var photoUrl = document.getElementById('photourl');
+  var notes = document.getElementById('notes');
+  formInputs.title = title.value;
+  formInputs.photoUrl = photoUrl.value;
+  formInputs.notes = notes.value;
+
 }
