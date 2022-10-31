940f7d7775427f2bdfd8d2b75382bbe3d42f5c7e
Giancarlo Massoni <massonicarlo55@gmail.com>
2022-10-31 21:48:31 +0000

    added nextEntryId incrementation to data model

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-var $photoUrl = document.querySelector('#photourl');
+var $photoUrl = document.querySelector('#photo-url');
 var $photoOutput = document.querySelector('img');
 var $form = document.querySelector('.entry-form');
+var nextEntryId = 0;
+var formInputs = {};
 
 $photoUrl.addEventListener('input', updatePhoto);
 $form.addEventListener('submit', updateForm);
@@ -13,12 +15,16 @@ function updatePhoto(event) {
 
 function updateForm(event) {
   event.preventDefault();
-  var formInputs = {};
+
   var title = document.getElementById('title');
-  var photoUrl = document.getElementById('photourl');
+  var photoUrl = document.getElementById('photo-url');
   var notes = document.getElementById('notes');
+
   formInputs.title = title.value;
   formInputs.photoUrl = photoUrl.value;
   formInputs.notes = notes.value;
+  formInputs.nextEntryId = nextEntryId;
+
+  nextEntryId++;
 
 }
