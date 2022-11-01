395b717d705fbb549e9c7fa881c146ace32e7f7e
Giancarlo Massoni <massonicarlo55@gmail.com>
2022-11-01 18:49:21 +0000

    Added DOM Tree creation for each entry and it appends

--- js/main.js
+++ js/main.js
@@ -28,3 +28,38 @@ function submitForm(event) {
   $previewImage.setAttribute('src', 'images/placeholder-image-square.jpg');
   data.nextEntryId++;
 }
+
+function renderEntry(entry) {
+  var row = document.createElement('div');
+  row.setAttribute('class', 'row');
+
+  var columnHalf = document.createElement('div');
+  columnHalf.setAttribute('class', 'column-half padding');
+  row.appendChild(columnHalf);
+
+  var img = document.createElement('img');
+  img.setAttribute('src', entry.photoUrl);
+  columnHalf.appendChild(img);
+
+  var columnHalfLine = document.createElement('div');
+  columnHalfLine.setAttribute('class', 'column-half line-height padding');
+  row.appendChild(columnHalfLine);
+
+  var h2 = document.createElement('h2');
+  h2.textContent = entry.title;
+  columnHalfLine.appendChild(h2);
+
+  var p = document.createElement('p');
+  p.textContent = entry.notes;
+  columnHalfLine.appendChild(p);
+
+  return row;
+
+}
+
+var $list = document.querySelector('.list');
+
+for (var i = 0; i < data.entries.length; i++) {
+  var $entry = renderEntry(data.entries[i]);
+  $list.appendChild($entry);
+}
